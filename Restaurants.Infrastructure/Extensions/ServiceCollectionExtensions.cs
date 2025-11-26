92eb2b19df3f24230178f97e234a81a03210598d
Research1127 <firdaucazizah@gmail.com>
2025-11-27 07:50:10 +0800

    initial commit with .gitignore file

---
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Infrastructure.Persistence;
+using Restaurants.Infrastructure.Seeders;
+
+namespace Restaurants.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("RestaurantsDbConn");
+        services.AddDbContext<RestaurantsDbContext>(options => options.UseNpgsql(connectionString));
+        
+        services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
+    }
+    
+}
---
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -0,0 +1,92 @@
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Persistence;
+using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurants.Infrastructure.Seeders;
+
+internal class RestaurantSeeder(RestaurantsDbContext dbContext) : IRestaurantSeeder
+{
+    public async Task Seed()
+    {
+        if (await dbContext.Database.CanConnectAsync())
+        {
+            if (!dbContext.Restaurants.Any())
+            {
+                var restaurants = GetRestaurants();
+                dbContext.Restaurants.AddRange(restaurants);
+                await dbContext.SaveChangesAsync();
+            }
+        }  
+    }
+
+    private IEnumerable<Restaurant> GetRestaurants()
+    {
+        List<Restaurant> restaurants = [
+        new()
+        {
+            Name = "KFC",
+            Category = "Food",
+            Description = "KFC restaurants",
+            ContactEmail = "kfc@gmail.com",
+            HasDelivery =  true,
+            Dishes = [
+            new()
+            {
+                Name = "Snack Plate",
+                Description = "Chicken and coslo",
+                Price = 10,
+            },
+            
+            new()
+            {
+            Name = "Chicken Tender",
+            Description = "Chicken and water",
+            Price = 14,
+            },
+            ],
+            
+            Address = new()
+            {
+                City = "Selangor",
+                Street = "Petaling Jaya",
+                PostalCode =  "98052",
+            }
+        },
+        new Restaurant()
+        {
+            Name = "McDonalds",
+            Category = "Food",
+            Description = "McDonalds restaurants",
+            ContactEmail = "McDonalds@gmail.com",
+            HasDelivery =  true,
+            Dishes = [
+                new()
+                {
+                    Name = "McDonalds Plate",
+                    Description = "Chicken and McDonalds",
+                    Price = 10,
+                },
+        
+                new()
+                {
+                    Name = "Chicken McDonalds",
+                    Description = "McDonalds and water",
+                    Price = 14,
+                },
+            ],
+        
+            Address = new()
+            {
+                City = "Kelantan",
+                Street = "Kota Bharu",
+                PostalCode =  "32343",
+            }
+        }
+        ];
+        
+        return restaurants;
+
+    }
+
+}
