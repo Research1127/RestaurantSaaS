33bef921b810d05662052a9227508f38eb95c9db
Research1127 <firdaucazizah@gmail.com>
2026-01-31 14:40:56 +0800

    Adding Presentation Layer and Clean Program.cs code

---
+++ Restaurants.Api/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,40 @@
+using Microsoft.OpenApi.Models;
+using Restaurants.Api.Middlewares;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace Restaurants.Api.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static void AddPresentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth" }
+                    },
+                    []
+                }
+            });
+        });
+
+        builder.Services.AddScoped<ErrorHandlingMiddleware>();
+        builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
+        builder.Host.UseSerilog((context, configuration) =>
+            configuration
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code,
+                    outputTemplate:"[{Timestamp:dd:MM:yyyy HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
+                .ReadFrom.Configuration(context.Configuration));
+    }
+}
--- Restaurants.Api/Program.cs
+++ Restaurants.Api/Program.cs
@@ -6,43 +6,22 @@ using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
 using Serilog.Sinks.SystemConsole.Themes;
+using Restaurants.Api.Extensions;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth" }
-            },
-            []
-        }
-    });
-});
 
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
+
+builder.AddPresentation();
+builder.Services.AddEndpointsApiExplorer();
+
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
-builder.Services.AddControllers();
-builder.Host.UseSerilog((context, configuration) =>
-    configuration
-        .WriteTo.Console(theme: AnsiConsoleTheme.Code,
-            outputTemplate:"[{Timestamp:dd:MM:yyyy HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
-        .ReadFrom.Configuration(context.Configuration));
+
         
 
 var app = builder.Build();
