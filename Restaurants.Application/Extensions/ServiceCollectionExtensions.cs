77e5e72550b41b656a242e48a28aab646f8a19a3
Research1127 <firdaucazizah@gmail.com>
2026-01-31 22:56:06 +0800

    Adding User Context to identify currently authenticated user

--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@ using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.Dishes;
 using Restaurants.Application.Restaurants;
+using Restaurants.Application.User;
 
 namespace Restaurants.Application.Extensions;
 
@@ -18,6 +19,9 @@ public static class ServiceCollectionExtensions
         services.AddAutoMapper(applicationAssembly);
         services.AddValidatorsFromAssembly(applicationAssembly)
             .AddFluentValidationAutoValidation();
+        
+        services.AddScoped<IUserContext, UserContext>();
+        services.AddHttpContextAccessor();
         
     }
 }
