1f887a1dfd4f0785f6d31176a9e80be007bc8ec6
Research1127 <firdaucazizah@gmail.com>
2026-02-07 19:05:20 +0800

    Add User Resources - only authorized users can modify their resources

--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -1,18 +1,29 @@
 using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Application.Users;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 
 public class CreateRestaurantCommandHandler(ILogger<CreateRestaurantCommandHandler> logger,
-    IMapper mapper,IRestaurantsRepository restaurantRepository) : IRequestHandler<CreateRestaurantCommand, int>
+    IMapper mapper,IRestaurantsRepository restaurantRepository,
+    IUserContext userContext) : IRequestHandler<CreateRestaurantCommand, int>
 {
     public async Task<int> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
     {
-        logger.LogInformation($"Creating a new Restaurant");
+        var currentuser = userContext.GetCurrentUser();
+        
+        
+        logger.LogInformation("{UserEmail} [{UserId}] is creating a new restaurant {@Restaurant}",
+            currentuser.Email,
+            currentuser.Id,
+            request);
         var restaurant = mapper.Map<Restaurant>(request);
+        
+        restaurant.OwnerId = currentuser.Id;
+        
         int id = await restaurantRepository.Create(restaurant);
         return id;
     }
--- Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -12,9 +12,17 @@ internal class RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> optio
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Restaurant>().OwnsOne(r => r.Address);
+        modelBuilder.Entity<Restaurant>()
+            .OwnsOne(r => r.Address);
 
-        modelBuilder.Entity<Restaurant>().HasMany(r => r.Dishes)
-            .WithOne().HasForeignKey(d => d.RestaurantId);
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId);
+        
+        modelBuilder.Entity<User>()
+            .HasMany(o => o.OwnedRestaurants)
+            .WithOne(r => r.Owner)
+            .HasForeignKey(r => r.OwnerId);
     }
 }
